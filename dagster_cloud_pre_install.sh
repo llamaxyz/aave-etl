68347e9a1e8d5b1b9a7b929f3ad0a81204d0f752
Scott Simpson <scottincrypto@gmail.com>
2023-01-23 07:46:27 +0000

    add env var for protobuf install error

--- dagster_cloud_pre_install.sh
+++ dagster_cloud_pre_install.sh
@@ -1,3 +1,8 @@
+# this resolves an install error with protobuf
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 pip3 install --upgrade pip
+
+# git is required for installing pip packages from github repos
+# gcc is required to install pyethash (ape dependency)
 apt update
 apt-get -y install git gcc
