174bce56d8cfe594b2d7f701a609d7077690eb9c
Scott Simpson <scottincrypto@gmail.com>
2023-02-15 10:16:47 +0000

    fix: change asset selection in sensor

--- financials/__init__.py
+++ financials/__init__.py
@@ -20,6 +20,9 @@ from financials.resources.bigquery_io_manager import bigquery_io_manager
 from dagster_gcp.gcs.io_manager import gcs_pickle_io_manager
 from dagster_gcp.gcs.resources import gcs_resource
 
+from dagster._utils import file_relative_path
+from dagster_dbt import dbt_cli_resource, load_assets_from_dbt_project
+
 from google.oauth2 import service_account
 from google.cloud import storage
 
@@ -43,10 +46,25 @@ financials_update_job = define_asset_job(
 
 financials_update_sensor = build_asset_reconciliation_sensor(
     name="financials_update_sensor",
-    asset_selection=AssetSelection.all(),
-    minimum_interval_seconds=60*30
+    asset_selection=AssetSelection.all() - AssetSelection.keys('block_numbers_by_day'),
+    minimum_interval_seconds=60*3
 )
 
+# ########################
+# # dbt config
+# ########################
+
+# DBT_PROJECT_DIR = file_relative_path(__file__, "../dbt_financials")
+# DBT_PROFILES_DIR = file_relative_path(__file__, "../dbt_financials/config")
+
+# dbt_assets = load_assets_from_dbt_project(
+#     DBT_PROJECT_DIR,
+#     io_manager_key="bq_io_manager"
+# )
+
+########################
+# logic for dev/prod environments
+########################
 # check for environment variable DAGSTER_DEPLOYMENT is set to a valid value
 if 'DAGSTER_DEPLOYMENT' in os.environ and os.environ['DAGSTER_DEPLOYMENT'] in ['local_filesystem', 'local_cloud', 'prod']:
     dagster_deployment = os.environ['DAGSTER_DEPLOYMENT']
