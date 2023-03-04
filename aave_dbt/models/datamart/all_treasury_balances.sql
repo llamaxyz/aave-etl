0ce59cf950d60ecf1c52190664c1b742cbb8479b
Scott Simpson <scottincrypto@gmail.com>
2023-03-04 20:13:32 +0000

    feat: add all_treasury_balances table

---
+++ aave_dbt/models/datamart/all_treasury_balances.sql
@@ -0,0 +1,72 @@
+{{ config(materialized='table') }}
+
+
+with usd_balance as (
+select 
+  block_day
+  , chain
+  , display_chain
+  , market
+  , display_name as display_market
+  , collector
+  , collector_label
+  , token
+  , symbol
+  , underlying_reserve
+  , underlying_reserve_symbol
+  , replace(measure, '_usd', '') as measure
+  , measure_type
+  , balance_group
+  , stable_class
+  , value as value_usd
+-- from datamart.all_financials
+from {{ref('all_financials')}}
+where 1=1
+  and measure in ('start_balance_usd', 'start_accrued_fees_usd')
+  and collector_label = 'Ethereum V2 Incentives Controller' -- excluded from treasury holdings, included in protocol holdings
+  and currency = 'usd'
+)
+
+, native_balance as (
+select 
+  block_day
+  , market
+  , collector
+  , token
+  , measure 
+  , value as value_native
+-- from datamart.all_financials
+from {{ref('all_financials')}}
+where 1=1
+  and measure in ('start_balance', 'start_accrued_fees')
+  and collector_label = 'Ethereum V2 Incentives Controller' -- excluded from treasury holdings, included in protocol holdings
+  and currency = 'native'
+)
+
+
+select 
+  u.block_day
+  , u.chain
+  , u.display_chain
+  , u.market
+  , u.display_market
+  , u.collector
+  , u.collector_label
+  , u.token
+  , u.symbol
+  , u.underlying_reserve
+  , u.underlying_reserve_symbol
+  , u.measure
+  , u.measure_type
+  , u.balance_group
+  , u.stable_class
+  , u.value_usd
+  , n.value_native
+from usd_balance u
+  left join native_balance n on (
+    u.block_day = n.block_day and
+    u.market = n.market and
+    u.collector = n.collector and 
+    u.token = n.token and
+    u.measure = n.measure
+  )
