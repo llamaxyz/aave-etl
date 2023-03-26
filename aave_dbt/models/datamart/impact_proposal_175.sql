f672542c3390873fc5c104855aa9d0e05800136e
Scott Simpson <scottincrypto@gmail.com>
2023-03-26 01:10:03 +0000

    feat: impact table for proposal 175

---
+++ aave_dbt/models/datamart/impact_proposal_175.sql
@@ -0,0 +1,60 @@
+{{ config(materialized='table') }}
+
+with wide_format as (
+SELECT 
+  s.block_day
+  , s.market
+  , s.reserve
+  , s.atoken_symbol
+  , s.atoken_supply - s.variable_debt - s.stable_debt as tvl
+  , (s.atoken_supply - s.variable_debt - s.stable_debt) * p.usd_price as tvl_usd
+  , s.atoken_supply as deposits
+  , s.atoken_supply * p.usd_price as deposits_usd
+  , s.variable_debt + s.stable_debt as borrows
+  , (s.variable_debt + s.stable_debt) * p.usd_price as borrows_usd
+  , n.revenue as revenue
+  , u.revenue as revenue_usd 
+  , p.usd_price
+-- FROM aave-prod.warehouse.market_state_by_day s
+FROM {{ source('warehouse','market_state_by_day')}} s
+--   left join (select * from aave-prod.datamart.all_revenue where currency = 'native' and token = '0x8dae6cb04688c62d939ed9b68d32bc62e49970b1') n on (
+  left join (select * from {{ref('all_revenue')}} where currency = 'native' and token = '0x8dae6cb04688c62d939ed9b68d32bc62e49970b1') n on (
+    s.block_day = n.block_day and
+    s.market = n.market and
+    s.reserve = n.underlying_reserve
+  )
+--   left join (select * from aave-prod.datamart.all_revenue where currency = 'usd' and token = '0x8dae6cb04688c62d939ed9b68d32bc62e49970b1') u on (
+  left join (select * from {{ref('all_revenue')}} where currency = 'usd' and token = '0x8dae6cb04688c62d939ed9b68d32bc62e49970b1') u on (
+    s.block_day = u.block_day and
+    s.market = u.market and
+    s.reserve = u.underlying_reserve
+  )
+--   left join financials_data_lake.aave_oracle_prices_by_day p on (
+  left join {{ source('financials_data_lake','aave_oracle_prices_by_day')}} p on (
+    s.block_day = p.block_day and
+    s.market = p.market and
+    s.reserve = p.reserve
+  )
+where 1=1
+  and s.market = 'ethereum_v2'
+  and s.reserve in ('0xd533a949740bb3306d119cc777fa900ba034cd52') -- (CRV)
+  and cast(s.block_day as date) > DATE_ADD(DATE '2023-03-13', INTERVAL -6 MONTH) -- 6 months before proposal
+)
+
+select 
+  *
+from wide_format
+unpivot (
+  value for measure in (
+    tvl
+    , tvl_usd
+    , deposits
+    , deposits_usd
+    , borrows
+    , borrows_usd
+    , revenue
+    , revenue_usd
+    , usd_price
+  )
+)
+order by block_day, measure
