a2532b7314218bbd2ee698034d312c82d16ff9c9
Scott Simpson <scottincrypto@gmail.com>
2023-04-23 08:29:32 +0000

    fix: remove dagster metadata

--- aave_dbt/models/datamart/market_state_by_day.sql
+++ aave_dbt/models/datamart/market_state_by_day.sql
@@ -1,7 +1,8 @@
 {{ config(materialized='table') }}
 
 select
-  *
+  *	
+  except (_dagster_partition_type, _dagster_partition_key, _dagster_partition_time, _dagster_load_timestamp)
 from {{ source('warehouse','market_state_by_day')}}
 order by market, atoken_symbol, block_day
 
