e126b78a2500d4086823ad3eb855a04df52d0c12
Scott Simpson <scottincrypto@gmail.com>
2023-05-25 09:17:02 +0000

    fix: sm covered markets align with snapshot votes

--- aave_dbt/models/datamart/sm_covered_markets_tvl_by_day.sql
+++ aave_dbt/models/datamart/sm_covered_markets_tvl_by_day.sql
@@ -7,15 +7,63 @@ select
   , tvl_usd
 -- from datamart.asset_tvl_by_day 
 from {{ ref('asset_tvl_by_day') }}
-where market in (
-  'ethereum_v1'
-  , 'ethereum_v2'
-  , 'ethereum_v3'
-  , 'aave_arc'
-  , 'avax_v2'
-  , 'polygon_v2'
-  , 'polygon_v3'
-)
+where 1=1 
+  and market in (
+    'ethereum_v1'
+    , 'ethereum_v2'
+  )
+  and block_day < '2022-08-01'
+union all 
+select 
+  block_day
+  , display_name || ' ' || display_chain as aave_market
+  , tvl_usd
+-- from datamart.asset_tvl_by_day 
+from {{ ref('asset_tvl_by_day') }}
+where 1=1
+  and market in (
+    'ethereum_v1'
+    , 'ethereum_v2'
+    , 'aave_arc'
+    , 'avax_v2'
+    , 'polygon_v2'
+  )
+  and block_day between '2022-08-01' and '2023-02-17'
+union all 
+select 
+  block_day
+  , display_name || ' ' || display_chain as aave_market
+  , tvl_usd
+-- from datamart.asset_tvl_by_day 
+from {{ ref('asset_tvl_by_day') }}
+where 1=1
+  and market in (
+    'ethereum_v1'
+    , 'ethereum_v2'
+    , 'ethereum_v3'
+    , 'aave_arc'
+    , 'avax_v2'
+    , 'polygon_v2'
+  )
+  and block_day between '2023-02-18' and '2023-03-15'
+union all 
+select 
+  block_day
+  , display_name || ' ' || display_chain as aave_market
+  , tvl_usd
+-- from datamart.asset_tvl_by_day 
+from {{ ref('asset_tvl_by_day') }}
+where 1=1
+  and market in (
+    'ethereum_v1'
+    , 'ethereum_v2'
+    , 'ethereum_v3'
+    , 'aave_arc'
+    , 'avax_v2'
+    , 'polygon_v2'
+    , 'polygon_v3'
+  )
+  and block_day > '2023-03-15'
 )
 
 select 
