be686ec4dbe3e9955bb39c80bb10bcd55d13d4aa
Scott Simpson <scottincrypto@gmail.com>
2023-05-25 19:36:09 +0000

    fix: support for atokens in grants dao table

--- aave_dbt/models/datamart/grants_dao_token_balances_by_day.sql
+++ aave_dbt/models/datamart/grants_dao_token_balances_by_day.sql
@@ -8,10 +8,15 @@ select
   , p.usd_price * e.balance as balance_usd
 -- from protocol_data_lake.erc20_balances_by_day e
 from {{ source('protocol_data_lake','erc20_balances_by_day')}} e
---   left join warehouse.token_prices_by_day p on (
+  -- left join datamart.aave_atokens a on (
+  left join {{ ref( 'aave_atokens' )}} a on (
+    e.chain = a.chain and
+    e.token_address = a.atoken
+  )
+  -- left join warehouse.token_prices_by_day p on (
   left join {{ source('warehouse','token_prices_by_day') }} p on (
     e.chain = p.chain and
-    e.token_address = p.reserve and 
+    coalesce(a.reserve, e.token_address) = p.reserve and 
     e.block_day = p.block_day
   )
 where e.wallet_address = '0x89c51828427f70d77875c6747759fb17ba10ceb0'
