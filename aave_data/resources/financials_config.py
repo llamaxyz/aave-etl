082a0c84195033c0826e39b741cfd2ae76878182
Scott Simpson <83217474+scottincrypto@users.noreply.github.com>
2023-08-27 10:46:15 +1000

    feat: add stablecoin pricing from coingecko

--- aave_data/resources/financials_config.py
+++ aave_data/resources/financials_config.py
@@ -887,7 +887,31 @@ COINGECKO_TOKENS = {
         "chain": "ethereum",
         "decimals": 18,
         "start_date": "2023-04-01",
-    }
+    },
+    "USDC": {
+        "cg_id": "usd-coin",
+        "symbol": "USDC",
+        "address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+        "chain": "ethereum",
+        "decimals": 6,
+        "start_date": "2022-01-01",
+    },
+    "USDT": {
+        "cg_id": "tether",
+        "symbol": "USDT",
+        "address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
+        "chain": "ethereum",
+        "decimals": 6,
+        "start_date": "2022-01-01",
+    },
+    "DAI": {
+        "cg_id": "dai",
+        "symbol": "DAI",
+        "address": "0x6b175474e89094c44da98b954eedeac495271d0f",
+        "chain": "ethereum",
+        "decimals": 18,
+        "start_date": "2022-01-01",
+    },
 }
 
 CONFIG_COMPOUND_v2 = {
